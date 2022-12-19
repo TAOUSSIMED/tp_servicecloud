20506eee32b0f8431bbe9e0e8939ae1f9d83b0c4
TAOUSSIMED <simotaoussi80@gmail.com>
2022-12-19 20:56:22 +0100

    frontend2

---
+++ ecom-web-app/src/app/products/products.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor(private http:HttpClient ) { }
+
+  ngOnInit(): void {
+    this.http.get()
+  }
+
+}
